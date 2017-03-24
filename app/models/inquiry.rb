9a751c2a40f815245ad0f6d98a91adbf404eb597
alindco <akl@alindco.com>
2017-03-24 19:42:00 +0000

    added login button to home page

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,19 +2,21 @@ class Inquiry < ApplicationRecord
   
   
   # belongs_to :user
+  belongs_to :user
   belongs_to :wedding 
   validates :wedding, presence: true
-  # has_many :emails
+  has_many :email_histories
+  
   
   accepts_nested_attributes_for :wedding
-  # accepts_nested_attributes_for :emails
+   accepts_nested_attributes_for :email_histories
    after_update :on_update
 
   # curl -X PATCH http://localhost:3000/inquiries/154 -D '{ inquiry: { closed: true } }'
   def on_update
     if closed_changed? && closed?
       puts "Closing Wedding related to this inquiry"
-      wedding.update!(status: 4)
+      wedding.update!(status: :closed)
   end
   end
 
