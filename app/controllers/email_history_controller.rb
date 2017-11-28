c13f1fb46a3a56947f4cc7fa89cc4e95c1130b86
Allan Lindgren <akl@alindco.com>
2017-11-27 19:42:25 -0600

    corected error in email_history controler

--- app/controllers/email_history_controller.rb
+++ app/controllers/email_history_controller.rb
@@ -1,11 +1,11 @@
 class EmailHistoryController < ApplicationController
 
 	def create
-		byebug
+		#byebug
 		type = params[:type]
 		template = params[:template_id]
 		id = params[:id]
-		if type] ='Request'
+		if type ='Request'
 			@item=Inquiry.find(id).history.new
 			@item.email_template_id = template_id
 			@item.date_sent = Dtae.today
