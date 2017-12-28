0144fa219119524b355c66b50e0de817822e581b
Allan Lindgren <akl@alindco.com>
2017-12-28 15:10:08 -0600

    remove byebug code

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -12,12 +12,11 @@ class InquiriesController < ApplicationController
     if @user.admin?
       @inquiries = Inquiry.where(closed: :isopen ).order("id")
     else
-      byebug
+      #byebug
      # @tmp = @user.inquiries.where(closed:  :isopen)
       @inquiries = Inquiry.where(closed: :isopen).where("inquiries.user_id = ?" ,[@user.id]).
       includes(:wedding).order("weddings.wedding_date")
-     # @inquiries = Inquiry.where(closed:  :isopen).where( user_id: @user.id ).includes(:wedding).order("wedding_date desc")
-      #@temp=@tmp.includes(:wedding).order("wedding_date desc")
+     
       
       
       # , closed: false
