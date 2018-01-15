57defc7bbb1d25c04708e066b39d95b44ce34c8b
Allan Lindgren <akl@alindco.com>
2018-01-15 13:26:38 -0600

    changed routes to not have confliction payment routes

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
   end
 
   resources :sessions
-  resources :payments
+  #resources :payments
   resources :inquiries do
     resources :email_templates
     resources :email_histories
